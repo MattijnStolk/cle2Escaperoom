bf6e76100ebbcf62989b97aec4400eb35d4071ac
Mattijn <0986087@hr.nl>
2020-12-12 16:55:36 +0100

    added pages
    Added simple create page with no db,
    Added indexAdmin with summary of reservations.

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 require_once "includes/database.php";
 
 
-
+$bbq = 1;
+mysqli_close($db);
 ?>
 <!doctype html>
 <html lang="en">
@@ -16,6 +17,41 @@ require_once "includes/database.php";
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-
+<!-- Hier komt de agenda met de becschikbare tijden -->
+<form action="<?= $_SERVER['REQUEST_URI']; ?>" method="post">
+    <div class="data-field">
+        <label for="fname">Voornaam</label>
+        <input id="fname" type="text" name="fname" value="<?= (isset($fname) ? $fname : ''); ?>"/>
+        <span><?= (isset($errors['fname']) ? $errors['fname'] : '') ?></span>
+    </div>
+    <div class="data-field">
+        <label for="lname">Achternaam</label>
+        <input id="lname" type="text" name="lname" value="<?= (isset($lname) ? $lname : ''); ?>"/>
+        <span><?= (isset($errors['lname']) ? $errors['lname'] : '') ?></span>
+    </div>
+    <div class="data-field">
+        <label for="email">Email</label>
+        <input id="email" type="email" name="email" value="<?= (isset($email) ? $email : ''); ?>"/>
+        <span><?= (isset($errors['email']) ? $errors['email'] : '') ?></span>
+    </div>
+    <div class="data-field">
+        <p>agenda met beschikbare datum en tijden</p>
+    </div>
+    <div class="data-field">
+        <label for="personamount">Aantal personen</label>
+        <input id="personamount" type="number" name="personamount" value="<?= (isset($personamount) ? $personamount : ''); ?>"/>
+        <span><?= (isset($errors['personamount']) ? $errors['personamount'] : '') ?></span>
+    </div>
+    <div>
+        <?php if ($bbq == 0) $bbq = '';
+        else $bbq = 'checked'?>
+        <label for="bbq">wilt u een bbq?</label>
+        <input id="bbq" type="checkbox" name="bbq" <?= $bbq ?>>
+        <span><?= (isset($errors['bbq']) ? $errors['bbq'] : '') ?></span>
+    </div>
+    <div class="data-submit">
+        <input type="submit" name="submit" value="Save"/>
+    </div>
+</form>
 </body>
 </html>
--- indexadmin.php
+++ indexadmin.php
@@ -13,8 +13,6 @@ while ($row = mysqli_fetch_assoc($result)) {
 
 //Close connection
 mysqli_close($db);
-
-print_r($reserveringen);
 ?>
 <!doctype html>
 <html lang="en">
@@ -23,9 +21,44 @@ print_r($reserveringen);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/adminstyles.css">
     <title>Document</title>
 </head>
 <body>
+    <table>
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>Naam</th>
+                <th>Email</th>
+                <th>datum</th>
+                <th>tijd</th>
+                <th>aantal personen</th>
+                <th>bbq?</th>
+                <th colspan="2"></th>
+            </tr>
+        </thead>
+        <tfoot>
+        <tr>
+            <td colspan="9">&copy; Reserveringen</td>
+        </tr>
+        </tfoot>
+        <tbody>
+        <?php foreach ($reserveringen as $reservering) { ?>
+                <?php if ($reservering['bbq'] == 0) $reservering['bbq'] = 'nee';
+                        else $reservering['bbq'] = 'ja'?>
+            <tr>
+                <td><?= $reservering['id'] ?></td>
+                <td><?= $reservering['fname'] ?> <?= $reservering['lname'] ?></td>
+                <td><?= $reservering['email'] ?></td>
+                <td><?= $reservering['date'] ?></td>
+                <td><?= $reservering['time'] ?></td>
+                <td><?= $reservering['personamount'] ?></td>
+                <td><?= $reservering['bbq'] ?></td>
 
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
 </body>
 </html>
