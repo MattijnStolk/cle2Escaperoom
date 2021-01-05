e298a8e192423b2753dc09c7ed9d9aba6beda4c1
Mattijn <0986087@hr.nl>
2021-01-05 14:25:20 +0100

    Update errorHandling.php

--- includes/errorHandling.php
+++ includes/errorHandling.php
@@ -12,3 +12,9 @@ if ($email == "") {
 if (!is_numeric($personAmount) || $personAmount == "") {
     $errors['personAmount'] = 'aantal pesonen mag niet leeg zijn en moet een nummer zijn.';
 }
+if ($personAmount < 0){
+    $errors['personAmount'] = 'Aantal personen mag niet negatief zijn';
+}
+if (!is_numeric($tel) || $tel == ""){
+    $errors['tel'] = 'Telefoonnummer mag niet leeg zijn en moeten cijfers zijn.';
+};
